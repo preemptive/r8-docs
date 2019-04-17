86c1d0a281a5e567542de945b0bfc056d12a428e
Peter Tefft <ptefft@preemptive.com>
2019-04-17 09:18:13 -0400

    Merged PR 3337: Merge ptefft/fix-internal-links to initial_docs
    Related work items: #18018

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,6 +10,30 @@ var sectionHeight = function() {
   }
 }
 
+function navigateToId(idExpression, instant) {
+  var elements = $(idExpression);
+  if (elements.length == 0) {
+      elements = $('[name="' + idExpression.substring(1) + '"]');
+      if (elements.length == 0) {
+        return;
+      }
+  }
+  var offset = $("#banner").is(":visible") ? 140 : 10;
+  var position = elements.offset().top - offset;
+  if (instant) {
+    window.scrollTo(0, position);
+  } else {
+    $("html, body").animate({scrollTop: position}, 400);
+  }
+}
+
+function navigateToHash(instant) {
+  var hash = document.location.hash;
+  if (hash && hash.length > 0) {
+    navigateToId(hash, instant);
+  }
+}
+
 $(window).resize(sectionHeight);
 
 $(function() {
@@ -19,15 +43,27 @@ $(function() {
     $("nav ul li:first-child a").parent().addClass("active");
   });
 
-  $("nav ul li").on("click", "a", function(event) {
-    var position = $($(this).attr("href")).offset().top - 190;
-    $("html, body").animate({scrollTop: position}, 400);
+  $('nav ul li').on("click", "a", function(event) {
+    navigateToId($(this).attr("href"));
+    event.preventDefault();
     $("nav ul li a").parent().removeClass("active");
     $(this).parent().addClass("active");
+  });
+
+  $('section a[href^="#"]').on("click", function(event) {
+    var href = navigateToId($(this).attr("href"));
     event.preventDefault();
   });
 
   sectionHeight();
 
   $('img').on('load', sectionHeight);
+
+  if (!window.performance || window.performance.navigation.type == 0) {
+    navigateToHash(true);
+  }
+  $(window).on("hashchange", function(event) {
+    navigateToHash(true);
+    event.preventDefault();
+  });
 });
